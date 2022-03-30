002124d6143b24e52e838657d1d549908fbc0c00
78741722Ale <pecorillaalessandro@gmail.com>
2022-03-30 15:19:12 +0200

    Snack 7 completo

---
+++ snack-7/assets/js/main.js
@@ -0,0 +1,17 @@
+/* Stampa le potenze di 2 fino a 1000. */
+// Dichiaro l'array vuoto in modo da poter mettere numeri al push.
+
+const arrayNumbers = [];
+console.log(arrayNumbers);
+
+// Stampato potenze fino a numero 1000 e non oltre
+for (let i = 1; i <= 10; i++) {
+    // Formula per arrivare fino a 1000
+    let number = (Math.pow(2, i));
+    // Stampa in console log
+    console.log(number);
+    // Messa nell'array
+    arrayNumbers.push["number"];
+    // Verifico in console log
+    console.log(arrayNumbers);
+}
