a7ec6bfbc705e00ceac5d0b1dfbcd062e7b13325
78741722Ale <pecorillaalessandro@gmail.com>
2022-03-30 12:48:24 +0200

    Snack 5 Completo

---
+++ snack-5/assets/js/main.js
@@ -0,0 +1,19 @@
+// Traccia
+
+/* 
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.
+*/
+
+for (let i = 0; i <= 6; i++) {
+   const userNumbers = parseInt(prompt("Inserisci un numero"));
+   // condizione pari o dispari
+    if (userNumbers % 2 != 0) {
+        const arrayNumbers = [userNumbers];
+        // Pusho i numeri dispari nell'array
+        arrayNumbers.push["userNumbers"];
+        //loggo in console
+        console.log(arrayNumbers);  
+    }
+}
