e251aa428ac3811dfe1d482a8502341e4cbbacfa
78741722Ale <pecorillaalessandro@gmail.com>
2022-03-30 12:51:08 +0200

    Snack 5 - Aggiunto array vuoto al di fuori della dichiarazione for

--- snack-5/assets/js/main.js
+++ snack-5/assets/js/main.js
@@ -6,6 +6,10 @@ Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array.
 */
 
+const arrayNumbers = []
+console.log(arrayNumbers);  
+
+
 for (let i = 0; i <= 6; i++) {
    const userNumbers = parseInt(prompt("Inserisci un numero"));
    // condizione pari o dispari
